8a78b56700340f432f25fcfdcfbb86198a968bdf
AlexKat <katsubo79@gmail.com>
2024-04-22 00:05:50 +0300

    main

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,10 @@
+// import axios from 'axios';
+// const URL = import.meta.env.VITE_API_BASE_URL;
+// export const api = axios.create({
+//   baseURL: URL,
+// });
 import axios from 'axios';
-const URL = import.meta.env.VITE_API_BASE_URL;
+
 export const api = axios.create({
-  baseURL: URL,
+  baseURL: 'https://661fa49416358961cd94fd6c.mockapi.io/',
 });
